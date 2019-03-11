fcc89e6e539f85cfa842ba061ee3fb4436fced77
Nikita Saydametov <cody1996@mail.ru>
2019-03-11 11:10:42 +0100

    oprava DU

--- uloha01.sh
+++ uloha01.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
-
 if [ "$1" == "--help" ]; then
   echo "Použití: uloha01.sh [--typ|--help] [cesta_k_souboru]
   --typ       Skript vypíše typ souboru (soubor, adresar, ...), jehož cestu určuje druhý parametr.
@@ -10,7 +8,7 @@ fi
 
 
 if [ "$1" == "--typ" ] && [ -e "$2" ]; then
-type=$(file -i "$2") && echo "$type" && exit 0
+echo "$(file -L "$2")" && exit 0
 fi
 
 
