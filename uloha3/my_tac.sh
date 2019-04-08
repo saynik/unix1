94e0c54146abac320dd9f5e3bf8db016708ca4ef
Nikita Saydametov <cody1996@mail.ru>
2019-04-09 00:42:53 +0200

    uloha3

---
+++ uloha3/my_tac.sh
@@ -0,0 +1,32 @@
+#/bin/bash 
+
+array=($*)
+
+revers_array () {
+    array_revers=("${@}")
+    min=0 ; max=$(( ${#array_revers[@]} - 1 ))
+    while [[ min -lt max ]]; do
+        x="${array_revers[$min]}"
+        array_revers[$min]="${array_revers[$max]}"
+        array_revers[$max]="$x"
+        (( min++, max-- ))
+    done
+    for i in "${array_revers[@]}" ; do echo $i ; done
+}
+
+n_revers_array () {
+    n=$1 ; cuts=0
+    revers_array "${array[@]:2}" &> /dev/null
+    tmp_array_revers=("${array_revers[@]}")
+    while [[ $cuts -le $(( ${#tmp_array_revers[@]} ))  ]] ; do
+        revers_array ${tmp_array_revers[@]:$cuts:$n}
+        cuts=$(( $cuts + $n ))
+    done
+}
+
+if [ "${1::1}" != "-" ]; then
+    revers_array "${array[@]}"
+elif [ "${1::1}""n" == "-n" ]; then
+    count=$2
+    n_revers_array $count
+fi
