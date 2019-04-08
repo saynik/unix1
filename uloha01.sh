eba4e3e65962dec2df01dbb3672357dcd1a483a8
Nikita Saydametov <cody1996@mail.ru>
2019-04-09 00:03:06 +0200

    v4

--- uloha01.sh
+++ uloha01.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-if [ "$1" == "--help" ]; then
-  echo "Použití: uloha01.sh [--typ|--help] [cesta_k_souboru]
-  --typ       Skript vypíše typ souboru (soubor, adresar, ...), jehož cestu určuje druhý parametr.
-  --help      Skript vypíše návod k použití tohoto programu"
-fi
+declare -A arr
+
+arr=( ['b']='block device'
+     ['c']='character device'
+     ['d']='folder'
+     ['p']='fifo'
+     ['L']='symbol link'
+     ['f']='file'
+     ['S']='socket' )
+
+info () {
+    printf "${@}\n"
+}
+
+cheeker () {
+    obj="$1" ;
+    for i in ${!arr[@]}; do 
+       if test "-$i" "$obj" ; then
+           info "${arr[$i]}" && exit 1
+       fi
+    done
+    info "File not found" && exit 1
+}
+
+main () {
+    if [ "$1" == "--typ" ] ; then
+        cheeker $2
+    elif [ "$1" == "--help" ] ; then
+        info "Usage: uloha01.sh [--typ | --help] [path to file]
+--typ check type object
+--help display this help and exit"
+    else 
+        info "Usage key '--help'"
+    fi
+}
 
 
-if [ "$1" == "--typ" ] && [ -e "$2" ]; then
-echo "$(file -b "$2")" && exit 0
-fi
-
-
-if [ "$1" == "--typ" ] && [ ! -e "$2" ]; then
-echo "Chyba" && exit 1
-fi
+main "$1" "$2"
