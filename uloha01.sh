f70e1e874d806c1606da55c62863959ae5fa3525
Wzwangel <43933007+Wzwangel@users.noreply.github.com>
2019-05-12 19:22:58 +0200

    v5
    Úplně přepracován způsob volání funkce a přidány funkce "len" a "subst"!

--- uloha01.sh
+++ uloha01.sh
@@ -1,40 +1,83 @@
 #!/bin/bash
 
-declare -A arr
+key="$1"
 
-arr=( ['b']='block device'
-     ['c']='character device'
-     ['d']='folder'
-     ['p']='fifo'
-     ['L']='symbol link'
-     ['f']='file'
-     ['S']='socket' )
+args=( )
 
-info () {
-    printf "${@}\n"
+for i in "$@" ; do
+    args+=( "$1" ) ; shift
+done
+
+keys=(
+    --normal
+    --reverse
+    --subst
+    --len
+    --help
+)
+
+action=(
+    normal_func
+    reverse_func
+    subst_func
+    len_func
+    help_func
+)
+
+help_info=(
+    "displays the arguments in direct order"
+    "displays the arguments in reverse order"
+    "variable substitution is called"
+    "displays the number of arguments"
+    "display this help and exit"
+)
+
+normal_func () {
+    for obj in "${args[@]}"; do
+        echo "$obj"
+    done
 }
 
-cheeker () {
-    obj="$1" ;
-    for i in ${!arr[@]}; do 
-       if test "-$i" "$obj" ; then
-           info "${arr[$i]}" && exit 1
-       fi
+reverse_func () {
+    min=0 ; max=$(( ${#args[@]} - 1 ))
+    while [[ min -lt max ]]; do
+        x="${args[$min]}"
+        args[$min]="${args[$max]}"
+        args[$max]="$x"
+        (( min++, max-- ))
     done
-    info "File not found" && exit 1
+    for i in "${args[@]}" ; do echo $i ; done
+ 
+}
+
+len_func () {
+    value_len=( )
+    for obj in "${args[@]}"; do
+        value_len+=( "${#obj}" )
+    done
+    echo ${value_len[@]}
+}
+
+subst_func () {
+    for obj in "${args[@]:3}"; do
+        echo $obj | sed "s/${args[@]:1:1}/${args[@]:2:1}/g"
+    done
+}
+
+help_func () {
+    echo "Syntax: viewer.sh [options] [args]"
+    for id in "${!keys[@]}" ; do
+        echo -e "\t ${keys[$id]} ${help_info[$id]}"
+    done 
 }
 
 main () {
-    if [ "$1" == "--typ" ] ; then
-        cheeker $2
-    elif [ "$1" == "--help" ] ; then
-        info "Usage: uloha01.sh [--typ | --help] [path to file]
---typ check type object
---help display this help and exit"
-    else 
-        info "Usage key '--help'"
-    fi
+    for id in ${!keys[@]}; do
+        if [ "${keys[id]}" == "$key" ] ; then
+            ${action[id]}
+            break
+        fi
+    done
 }
 
-
-main "$1" "$2"
+main
